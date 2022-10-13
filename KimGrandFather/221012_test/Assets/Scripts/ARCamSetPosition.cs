89756e62614f7f4257220d2d2b598b0148ce8afd
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-10-13 11:15:53 +0900

    높이검출 테스트용 프로젝트 생성

--- KimGrandFather/221012_test/Assets/Scripts/ARCamSetPosition.cs
+++ KimGrandFather/221012_test/Assets/Scripts/ARCamSetPosition.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARCamSetPosition : MonoBehaviour
 {
-    
+    private int _count = 0;
     [SerializeField] private GameObject ARCam;
 
     
-    private Vector3 RaycastHitPosition()
+    private void Update() 
     {
-        Vector3 position;
+        if(_count < 120)
+        {
+            SetCameraPosition();
+        }
+        //ㅆㅂ
+    }
+    
 
-        
+    private void SetCameraPosition()
+    {
+        if(RaycastHitPositionY() > 0.5f)
+        {
+            ARCam.transform.position = new Vector3(ARCam.transform.position.x, ARCam.transform.position.y - RaycastHitPositionY(), ARCam.transform.position.z);
+            _count++;
+        }
+    }
+
+    private float RaycastHitPositionY()
+    {
+        float Y = 0f;
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        if(hits.Count > 0)
+        {
+            if(arRaycaster.Raycast(ARCam.transform.forward, hits, TrackableType.Planes))
+            {
+                Pose LastHit = hits[hits.Count - 1].pose;
+                Y = LastHit.position.y;
+            }
+        }
+    
+        return Y;
     }
 }
